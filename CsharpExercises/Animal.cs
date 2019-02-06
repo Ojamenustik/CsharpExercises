fbd6c4de0ecce075d6330017cfd5a10c84e32cf9
Ojamenustik <baka.yarichin@gmail.com>
2019-02-06 18:43:20 +0100

    first commit, C# tutorial by Derek Banas

---
+++ CsharpExercises/Animal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExercises
+{
+    class Animal
+    {
+        public double height { get; set; }
+        public double weight { get; set; }
+        public string sound { get; set; }
+
+        public string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Animal()
+        {
+            this.height = 0;
+            this.weight = 0;
+            this.sound = "No sound";
+            this.name = "No name";
+            NumOfAnimals++;
+        }
+
+        public Animal(double height, double weight, string name, string sound)
+        {
+            this.height = height;
+            this.weight =weight;
+            this.sound = sound;
+            this.name = name;
+            NumOfAnimals++;
+        }
+
+        private static int NumOfAnimals = 0;
+
+        public static int getNumOfAnimals()
+        {
+            return NumOfAnimals;
+        }
+
+        public string toString()
+        {
+            return String.Format("{0} is {1} inches tall, weighs {2} lbs and likes to say {3}.", name, height, weight,
+                sound);
+        }
+    }
+}
--- CsharpExercises/Program.cs
+++ CsharpExercises/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,210 @@ namespace CsharpExercises
     {
         static void Main(string[] args)
         {
+            
+            /* 
+            Console.WriteLine("What is your name ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello "+name);
+            Console.ReadLine();
+            
+            int maxInt = int.MaxValue;
+            long maxLong = long.MaxValue;
+            decimal maxDec = decimal.MaxValue;
+            float maxFloat = float.MaxValue;
+            double maxDouble = double.MaxValue;
+
+            Console.WriteLine(maxInt);
+            Console.WriteLine(maxLong);
+            Console.WriteLine(maxDec);
+            Console.WriteLine(maxFloat);
+            Console.WriteLine(maxDouble);
+           
+            var anotherName = "Tom";
+            Console.WriteLine("Another name is a {0}", anotherName);
+
+            double number1 = 10.5;
+            double number2 = 15;
+
+            Console.WriteLine("Math.Abs(number1) "+ (Math.Abs(number1)));
+            Console.WriteLine("Math.Ceiling(number1) "+ (Math.Ceiling(number1)));
+            Console.WriteLine("Math.Floor(number1) "+ (Math.Floor(number1)));
+            Console.WriteLine("Math.Max(number1, number2) "+ (Math.Max(number1, number2)));
+            Console.WriteLine("Math.Min(number1, number2) "+ (Math.Min(number1, number2)));
+            Console.WriteLine("Math.Pow(number1)//podnoszenie do potegi "+ (Math.Pow(number1,2)));
+            Console.WriteLine("Math.Round(number1) "+ (Math.Round(number1)));
+            Console.WriteLine("Math.Sqrt(number1) "+ (Math.Sqrt(number1)));
+            
+
+            //Random Numbers
+
+            Random rand = new Random();
+
+
+            //Relational Operators: > < >= <= == !=
+            //Logical Operators: && || ^ !
+
+            int age = rand.Next(1, 19);
+            Console.WriteLine("Random Number between 1 and 18: " + age);
+
+            if ((age >= 5) && (age <= 7))
+            {
+                Console.WriteLine("Go to elemantary school");
+            }
+            else if ((age > 7) && (age < 13))
+            {
+                Console.WriteLine("Go to middle school");
+            }
+            else
+            {
+                Console.WriteLine("Go home");
+            }
+
+            bool canDrive = age >= 16 ? true : false;
+
+            switch (age)
+            {
+                case 0:
+                    Console.WriteLine("Infant");
+                    break;
+                case 1:
+                    Console.WriteLine("Toddler");
+                    break;
+                default:
+                    Console.WriteLine("Child");
+                    break;
+            }
+
+            string guess;
+            do
+            {
+                Console.WriteLine("Guess a Number");
+                guess = Console.ReadLine();
+            } while (!guess.Equals("15"));
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                if ((i % 2) > 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            string randStr = " Uh oh ah random krach";
+            foreach (char a in randStr)
+            {
+                Console.WriteLine(a);
+            }
+
+            /* \'  \"  \\  \b  \n  \t  */
+            
+            /*
+            Console.WriteLine("Empty? " + String.IsNullOrEmpty(randStr));
+            Console.WriteLine("Empty or just white spaces? " + String.IsNullOrWhiteSpace(randStr));
+            Console.WriteLine("Length: " + randStr.Length);
+
+            Console.WriteLine("Index of \"oh\" {0}", randStr.IndexOf("oh"));
+            Console.WriteLine("2nd word: {0}", randStr.Substring(3, 4));
+
+            bool b = randStr.StartsWith("a");
+            bool b2 = randStr.EndsWith("h");
+
+            Console.WriteLine("Trim:{0}", randStr.Trim()); //TrimEnd(), TrimStart()
+            Console.WriteLine("Replace: {0}", randStr.Replace("random", "super"));
+            Console.WriteLine("Remove: {0}", randStr.Remove(0, 3));
+            */
+            string[] names = new string[3] {"Matt", "Joe", "Paul"};
+            int[] MyArray = {1, 2, 3, 4};
+
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                Console.WriteLine("{0}: {1}", i, MyArray[i]);
+            }
+
+            foreach(int a in MyArray)
+            {
+
+                Console.WriteLine(a);
+            }
+
+            //Multidimential arrays
+
+            int[,] multArray = new int[5, 3];
+            int[,] multArray2 = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+
+            for (int i = 0; i < multArray2.GetLength(0); i++)
+            {
+                for (int j = 0; j < multArray2.GetLength(1); j++)
+                {
+                    Console.WriteLine("{0} | {1} : {2}", i,j, multArray2[i,j]);
+                }
+            }
+
+            //List - resize for you
+
+            List<int> numList = new List<int>();
+
+            numList.Add(5);
+            numList.Add(25);
+            numList.Add(35);
+
+            int[] randArray = {1, 2, 3, 4, 5};
+            numList.AddRange(randArray);
+
+            List<int> numList2 = new List<int>(randArray);
+            List<int> numList3 = new List<int>(new int[]{1,2,3,4,4,5,6,7});
+            numList.Insert(5,10);
+            //numList.Remove(5);
+
+            for (int i = 0; i< numList.Count; i++)
+            {
+                Console.WriteLine(numList[i]);
+            }
+
+            Console.WriteLine("4 is in index: "+numList.IndexOf(4));
+            Console.WriteLine("5 in List: "+numList.Contains(5));
+
+            List<string> strList = new List<string>(new String[] { "Tom", "Jerry" });
+
+            Console.WriteLine("Tom in list "+strList.Contains("tom", StringComparer.OrdinalIgnoreCase));
+
+            strList.Sort(); //alfabetycznie
+
+            for (int i = 0; i < strList.Count; i++)
+            {
+                Console.WriteLine(strList[i]);
+            }
+
+            //Exceptions
+
+            try
+            {
+                Console.Write("Divide 10 by ");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine("10 / {0} = {1}", num, (10 / num));
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Can't divide by zero!");
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+      
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+            }
+
+            //Classes
+
+            Animal spot = new Animal(15,10,"Spot", "Woof");
+            Console.WriteLine("{0} says {1}.",spot.name, spot.sound);
+            Console.WriteLine("Number of animals: " + Animal.getNumOfAnimals());
+            Console.WriteLine(spot.toString());
+
+            Console.ReadLine();
         }
     }
 }
